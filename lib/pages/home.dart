488b4df988cc8c5d2d2e3704a83d508a761e650b
Siddhant Tomar <siddhanttomar@hotmail.com>
2021-02-09 00:11:54 +0530

    DAY8 completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,29 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("MyApp"),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        fixedColor: Colors.deepPurple,
+        items: [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: "Search",
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            label: "Profile",
+            icon: Icon(Icons.account_box),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.navigation_rounded),
+        backgroundColor: Colors.deepPurpleAccent,
+        onPressed: () {},
+      ),
       body: Center(
         child: Container(
           child: Text("Welcome to $days MyApp $name"),
