487db51d6c9209f927215225193dfdbe177cfd43
Siddhant Tomar <siddhanttomar@hotmail.com>
2021-02-28 13:24:56 +0530

    editingthwNEWABOUTPAGE

--- lib/pages/toabout.dart
+++ lib/pages/toabout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project1/widgets/themes.dart';
 
 class About extends StatefulWidget {
   @override
@@ -8,10 +10,31 @@ class About extends StatefulWidget {
 class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child: Container(
-          child: Text("About us"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About us"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/parlour-1590994911.jpg",
+                fit: BoxFit.cover,
+              ),
+              Text(
+                "Hello We are B make overs salon and we are one of the fastest growing unisex salon in Gr,NOida we are situated  in one of the  finestr society named Omaxe palm green. ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: GoogleFonts.openSansCondensed().fontFamily,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'themes.dart';
 import 'package:project1/utils/routes.dart';
+import 'package:flutter/src/widgets/navigator.dart';
 
 class MyDrawer extends StatefulWidget {
   @override
@@ -8,73 +9,70 @@ class MyDrawer extends StatefulWidget {
 }
 
 class _MyDrawerState extends State<MyDrawer> {
- 
   @override
   Widget build(BuildContext context) {
-    
     return Drawer(
-       child: Container(
-          color: MyTheme.creameColor,
-          child: ListView(
-            children: [
-              DrawerHeader(
-                padding: EdgeInsets.zero,
-                child: UserAccountsDrawerHeader(
-                  accountName: Text("BmakeOvers"),
-                  accountEmail: Text("siddhanttomar@hotmail.com"),
-                  decoration: BoxDecoration(color: MyTheme.darkblueishColor),
-                  currentAccountPicture: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/163214005/original/b502735733b0164118ac56625455332e885c1cbf/design-your-unisex-salon-logo.png"),
-                  ),
+      child: Container(
+        color: MyTheme.creameColor,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                accountName: Text("BmakeOvers"),
+                accountEmail: Text("siddhanttomar@hotmail.com"),
+                decoration: BoxDecoration(color: MyTheme.darkblueishColor),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/163214005/original/b502735733b0164118ac56625455332e885c1cbf/design-your-unisex-salon-logo.png"),
                 ),
               ),
-              ListTile(
-                onTap: () => MyRoutes.toabout,
-                leading: Icon(
-                  Icons.border_color,
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pushNamed(context, MyRoutes.toabout);
+              },
+              leading: Icon(
+                Icons.border_color,
+                color: Colors.black,
+              ),
+              title: Text(
+                "About",
+                textScaleFactor: 1.2,
+                style: TextStyle(
                   color: Colors.black,
-                ),
-                title: Text(
-                  "About",
-                  textScaleFactor: 1.2,
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
                 ),
               ),
-              ListTile(
-                leading: Icon(
-                  Icons.account_circle_rounded,
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.account_circle_rounded,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Staff",
+                textScaleFactor: 1.2,
+                style: TextStyle(
                   color: Colors.black,
-                ),
-                title: Text(
-                  "Staff",
-                  textScaleFactor: 1.2,
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
                 ),
               ),
-              ListTile(
-                
-                leading: Icon(
-                  Icons.phone,
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.phone,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Contact",
+                textScaleFactor: 1.2,
+                style: TextStyle(
                   color: Colors.black,
                 ),
-                title: Text(
-                  "Contact",
-                  textScaleFactor: 1.2,
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
-      
+      ),
     );
-    
   }
 }
